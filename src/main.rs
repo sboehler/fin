c28b9f205748f539f5cf0400d96e3b0f97a2f717
Silvio Böhler <silvio@monadic.ch>
2020-08-14 14:05:18 +0200

    Iterating through code points

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate unicode_reader;
+use unicode_reader::CodePoints;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "journal.bean";
+    let file = std::fs::File::open(path).expect("Could not open file");
+    let p = Parser {
+        codepoints: CodePoints::from(file),
+    };
+    for g in p.codepoints {
+        print!("{}", g.unwrap());
+    }
+}
+
+struct Parser {
+    codepoints: CodePoints<std::io::Bytes<std::fs::File>>,
 }
