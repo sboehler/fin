72d4a140f06b3dd83a01da90fe9db255fb8d470f
Silvio Böhler <silvio@monadic.ch>
2020-08-20 00:13:16 +0200

    Create a library

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+extern crate fin;
 extern crate unicode_reader;
+use fin::scanner::Scanner;
 use std::fs::File;
-
-mod model;
-mod scanner;
 
 fn main() {
     let path = "journal.bean";
     let file = File::open(path).expect("Could not open file");
-    let p = scanner::Scanner::new(file);
+    let p = Scanner::new(file);
     for g in p {
         print!("{}", g.unwrap());
     }
