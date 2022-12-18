d031850b2f9ff9ade783fb795ed36f24ae9638c2
Silvio Böhler <sboehler@noreply.users.github.com>
2022-12-19 00:20:09 +0100

    Pass error directly

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ struct Cli {
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     match &cli.command {
-        commands::Commands::Print(p) => p.run()?,
-    };
-    Ok(())
+        commands::Commands::Print(p) => p.run(),
+    }
 }
