87d8ff0fbfbc70fe56685991acb1e4e9b9ce8db6
Silvio Böhler <sboehler@noreply.users.github.com>
2024-05-11 17:26:08 +0200

    Ephasize character-orientation of Scanner

--- src/syntax/scanner2.rs
+++ src/syntax/scanner2.rs
@@ -1,4 +1,4 @@
-use super::cst::{Rng, Token};
+use super::cst::Rng;
 use super::file::File;
 use std::rc::Rc;
 use std::{cell::RefCell, iter::Peekable, str::CharIndices};
@@ -15,11 +15,56 @@ pub struct ScannerError {
     pub file: Rc<File>,
     pub pos: usize,
     pub msg: Option<String>,
-    pub want: Token,
-    pub got: Token,
+    pub want: Character,
+    pub got: Character,
 }
 
 pub type Result<T> = std::result::Result<T, ScannerError>;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Character {
+    EOF,
+    Char(char),
+    NotChar(char),
+    Digit,
+    AlphaNum,
+    Any,
+    HorizontalSpace,
+    NewLine,
+    OneOf(Vec<Character>),
+}
+
+impl Character {
+    pub fn from_char(ch: Option<char>) -> Self {
+        match ch {
+            None => Self::EOF,
+            Some('\n') => Self::NewLine,
+            Some(c) if c.is_whitespace() => Self::HorizontalSpace,
+            Some(c) => Self::Char(c),
+        }
+    }
+
+    pub fn is(&self, o: Option<char>) -> bool {
+        match o {
+            None => match self {
+                Character::EOF => true,
+                Character::NewLine => true,
+                _ => false,
+            },
+            Some(c) => match self {
+                Character::EOF => false,
+                Character::Char(a) => c == *a,
+                Character::NotChar(a) => c != *a,
+                Character::Digit => c.is_ascii_digit(),
+                Character::AlphaNum => c.is_alphanumeric(),
+                Character::Any => true,
+                Character::HorizontalSpace => c.is_ascii_whitespace() && c != '\n',
+                Character::NewLine => c == '\n',
+                Character::OneOf(cs) => cs.iter().any(|c| c.is(o)),
+            },
+        }
+    }
+}
 
 struct Scope<'a, 'b> {
     s: &'a Scanner<'b>,
@@ -28,7 +73,7 @@ struct Scope<'a, 'b> {
 }
 
 impl<'a, 'b> Scope<'a, 'b> {
-    fn error(&self, want: Token, got: Token) -> ScannerError {
+    fn error(&self, want: Character, got: Character) -> ScannerError {
         ScannerError {
             file: self.s.source.clone(),
             pos: self.s.pos(),
@@ -81,109 +126,67 @@ impl<'a> Scanner<'a> {
             .map_or_else(|| self.source.text.len(), |t| t.0)
     }
 
-    pub fn read_while_1<P>(&self, token: Token, pred: P) -> Result<Rng>
-    where
-        P: Fn(char) -> bool,
-    {
+    pub fn read_while_1(&self, ch: Character) -> Result<Rng> {
         let scope = self.scope();
-        if !self.current().map_or(false, &pred) {
-            return Err(scope.error(token, Token::from_char(self.current())));
+        if !ch.is(self.current()) {
+            return Err(scope.error(ch, Character::from_char(self.current())));
         }
-        Ok(self.read_while(pred))
+        Ok(self.read_while(ch))
     }
 
-    pub fn read_while<P>(&self, pred: P) -> Rng
-    where
-        P: Fn(char) -> bool,
-    {
+    pub fn read_while(&self, ch: Character) -> Rng {
         let scope = self.scope();
-        while self.current().map_or(false, &pred) {
+        while ch.is(self.current()) {
             self.advance();
         }
         scope.rng()
     }
 
-    pub fn read_until<P>(&self, pred: P) -> Rng
-    where
-        P: Fn(char) -> bool,
-    {
-        self.read_while(|v| !pred(v))
+    pub fn read_until(&self, ch: Character) -> Rng {
+        let scope = self.scope();
+        while !ch.is(self.current()) {
+            self.advance();
+        }
+        scope.rng()
     }
 
     pub fn read_all(&self) -> Rng {
-        self.read_while(|_| true)
+        self.read_while(Character::Any)
     }
 
-    pub fn read_char(&self, c: char) -> Result<Rng> {
+    pub fn read_char(&self, ch: Character) -> Result<Rng> {
         let scope = self.scope();
-        match self.current() {
-            Some(d) if c == d => {
-                self.advance();
-                Ok(scope.rng())
-            }
-            o => Err(scope.error(Token::Char(c), Token::from_char(o))),
+        if ch.is(self.current()) {
+            self.advance();
+            Ok(scope.rng())
+        } else {
+            Err(scope.error(ch, Character::from_char(self.current())))
         }
     }
 
     pub fn read_string(&self, str: &str) -> Result<Rng> {
         let scope = self.scope();
         for c in str.chars() {
-            self.read_char(c)?;
+            self.read_char(Character::Char(c))?;
         }
         Ok(scope.rng())
-    }
-
-    pub fn read_identifier(&self) -> Result<Rng> {
-        let scope = self.scope();
-        if self.read_while(char::is_alphanumeric).is_empty() {
-            Err(scope.error(Token::AlphaNum, Token::from_char(self.current())))
-        } else {
-            Ok(scope.rng())
-        }
     }
 
     pub fn read_1(&self) -> Result<Rng> {
         let scope = self.scope();
         match self.advance() {
             Some(_) => Ok(scope.rng()),
-            None => Err(scope.error(Token::Any, Token::EOF)),
+            None => Err(scope.error(Character::Any, Character::EOF)),
         }
     }
 
-    pub fn read_1_with<P>(&self, token: Token, pred: P) -> Result<Rng>
-    where
-        P: Fn(char) -> bool,
-    {
+    pub fn read_n(&self, n: usize, token: Character) -> Result<Rng> {
         let scope = self.scope();
-        match self.current() {
-            Some(c) if pred(c) => {
-                self.advance();
-                Ok(scope.rng())
+        for _ in 0..n {
+            if !token.is(self.current()) {
+                return Err(scope.error(token, Character::from_char(self.current())));
             }
-            Some(c) => Err(scope.error(token, Token::Char(c))),
-            None => Err(scope.error(token, Token::EOF)),
-        }
-    }
-
-    pub fn read_n_with<P>(&self, n: usize, token: Token, pred: P) -> Result<Rng>
-    where
-        P: Fn(char) -> bool,
-    {
-        let scope = self.scope();
-        for _ in 0..n {
-            match self.current() {
-                Some(c) if pred(c) => self.advance(),
-                Some(c) => return Err(scope.error(token, Token::Char(c))),
-                None => return Err(scope.error(token, Token::EOF)),
-            };
-        }
-        Ok(scope.rng())
-    }
-
-    pub fn read_n(&self, n: usize) -> Result<Rng> {
-        let scope = self.scope();
-        for _ in 0..n {
-            self.read_1()?;
+            self.advance();
         }
         Ok(scope.rng())
     }
@@ -197,34 +200,34 @@ impl<'a> Scanner<'a> {
             }
             Some(ch) => Err(self.error(
                 None,
-                Token::Either(vec![Token::Char('\n'), Token::EOF]),
-                Token::Char(ch),
+                Character::OneOf(vec![Character::Char('\n'), Character::EOF]),
+                Character::Char(ch),
             )),
         }
     }
 
-    pub fn read_space1(&self) -> Result<Rng> {
+    pub fn read_space_1(&self) -> Result<Rng> {
         let scope = self.scope();
         match self.current() {
             Some(ch) if !ch.is_ascii_whitespace() => {
-                Err(scope.error(Token::WhiteSpace, Token::Char(ch)))
+                Err(scope.error(Character::HorizontalSpace, Character::Char(ch)))
             }
             _ => Ok(self.read_space()),
         }
     }
 
     pub fn read_space(&self) -> Rng {
-        self.read_while(|c| c != '\n' && c.is_ascii_whitespace())
+        self.read_while(Character::HorizontalSpace)
     }
 
     pub fn read_rest_of_line(&self) -> Result<Rng> {
         let scope = self.scope();
-        self.read_while(|c| c.is_whitespace() && c != '\n');
+        self.read_while(Character::HorizontalSpace);
         self.read_eol()?;
         Ok(scope.rng())
     }
 
-    pub fn error(&self, msg: Option<String>, want: Token, got: Token) -> ScannerError {
+    pub fn error(&self, msg: Option<String>, want: Character, got: Character) -> ScannerError {
         ScannerError {
             file: self.source.clone(),
             pos: self.pos(),
@@ -244,9 +247,9 @@ mod test_scanner {
     fn test_read_while() {
         let mem = File::mem("aaasdff");
         let s = Scanner::new(&mem);
-        assert_eq!("aaasd", s.read_while(|c| c != 'f').text());
-        assert_eq!("ff", s.read_while(|c| c == 'f').text());
-        assert_eq!("", s.read_while(|c| c == 'q').text());
+        assert_eq!("aaasd", s.read_while(Character::NotChar('f')).text());
+        assert_eq!("ff", s.read_while(Character::Char('f')).text());
+        assert_eq!("", s.read_while(Character::Char('q')).text());
         assert_eq!(Ok(""), s.read_eol().as_ref().map(Rng::text));
     }
 
@@ -256,25 +259,23 @@ mod test_scanner {
         let s = Scanner::new(&f);
         assert_eq!(
             Ok("aaasd"),
-            s.read_while_1(Token::Any, |c| c != 'f')
+            s.read_while_1(Character::NotChar('f'))
                 .as_ref()
                 .map(Rng::text)
         );
         assert_eq!(
             Ok("ff"),
-            s.read_while_1(Token::Char('f'), |c| c == 'f')
-                .as_ref()
-                .map(Rng::text)
+            s.read_while_1(Character::Char('f')).as_ref().map(Rng::text)
         );
         assert_eq!(
             Err(ScannerError {
                 file: f.clone(),
                 pos: 7,
                 msg: None,
-                want: Token::Char('q'),
-                got: Token::EOF
+                want: Character::Char('q'),
+                got: Character::EOF
             }),
-            s.read_while_1(Token::Char('q'), |c| c == 'q')
+            s.read_while_1(Character::Char('q'))
         );
         assert_eq!("", s.read_eol().unwrap().text());
     }
@@ -283,20 +284,20 @@ mod test_scanner {
     fn test_read_char() {
         let f = File::mem("asdf");
         let s = Scanner::new(&f);
-        assert_eq!("a", s.read_char('a').unwrap().text());
+        assert_eq!("a", s.read_char(Character::Char('a')).unwrap().text());
         assert_eq!(
             Err(ScannerError {
                 file: f.clone(),
                 pos: 1,
                 msg: None,
-                want: Token::Char('q'),
-                got: Token::Char('s')
+                want: Character::Char('q'),
+                got: Character::Char('s')
             }),
-            s.read_char('q')
+            s.read_char(Character::Char('q'))
         );
-        assert_eq!("s", s.read_char('s').unwrap().text());
-        assert_eq!("d", s.read_char('d').unwrap().text());
-        assert_eq!("f", s.read_char('f').unwrap().text());
+        assert_eq!("s", s.read_char(Character::Char('s')).unwrap().text());
+        assert_eq!("d", s.read_char(Character::Char('d')).unwrap().text());
+        assert_eq!("f", s.read_char(Character::Char('f')).unwrap().text());
         assert_eq!("", s.read_eol().unwrap().text());
     }
 
@@ -310,24 +311,12 @@ mod test_scanner {
                 file: s.source.clone(),
                 pos: 2,
                 msg: None,
-                want: Token::Char('q'),
-                got: Token::Char('d')
+                want: Character::Char('q'),
+                got: Character::Char('d')
             }),
-            s.read_char('q')
+            s.read_char(Character::Char('q'))
         );
         assert_eq!(Ok("df"), s.read_string("df").as_ref().map(Rng::text));
-        assert_eq!(Ok(""), s.read_eol().as_ref().map(Rng::text));
-    }
-
-    #[test]
-    fn test_read_identifier() {
-        let f = File::mem("foo bar 1baz");
-        let s = Scanner::new(&f);
-        assert_eq!(Ok("foo"), s.read_identifier().as_ref().map(Rng::text));
-        assert_eq!(" ", s.read_while(|c| c.is_ascii_whitespace()).text());
-        assert_eq!(Ok("bar"), s.read_identifier().as_ref().map(Rng::text));
-        assert_eq!(" ", s.read_while(|c| c.is_ascii_whitespace()).text());
-        assert_eq!(Ok("1baz"), s.read_identifier().as_ref().map(Rng::text));
         assert_eq!(Ok(""), s.read_eol().as_ref().map(Rng::text));
     }
 
@@ -343,8 +332,8 @@ mod test_scanner {
                 file: File::mem("\n\n  \nfoo"),
                 pos: 5,
                 msg: None,
-                want: Token::Either(vec![Token::Char('\n'), Token::EOF]),
-                got: Token::Char('f')
+                want: Character::OneOf(vec![Character::Char('\n'), Character::EOF]),
+                got: Character::Char('f')
             }),
             s.read_rest_of_line()
         );
@@ -364,8 +353,8 @@ mod test_scanner {
                 file: s.source.clone(),
                 pos: 3,
                 msg: None,
-                want: Token::Any,
-                got: Token::EOF
+                want: Character::Any,
+                got: Character::EOF
             }),
             s.read_1()
         );
@@ -376,49 +365,35 @@ mod test_scanner {
     fn test_read_1_with() {
         let f = File::mem("asdf");
         let s = Scanner::new(&f);
-        assert_eq!(
-            "a",
-            s.read_1_with(Token::Char('a'), |c| c == 'a')
-                .unwrap()
-                .text()
-        );
-        assert_eq!(
-            "s",
-            s.read_1_with(Token::Custom("no a".into()), |c| c != 'a')
-                .unwrap()
-                .text()
-        );
+        assert_eq!("a", s.read_char(Character::Char('a')).unwrap().text());
+        assert_eq!("s", s.read_char(Character::NotChar('a')).unwrap().text());
         assert_eq!(
             Err(ScannerError {
                 file: s.source.clone(),
                 pos: 2,
                 msg: None,
-                want: Token::Digit,
-                got: Token::Char('d')
+                want: Character::Digit,
+                got: Character::Char('d')
             }),
-            s.read_1_with(Token::Digit, |c| c.is_ascii_digit())
+            s.read_char(Character::Digit)
         );
         assert_eq!(
             Ok("d"),
-            s.read_1_with(Token::Char('d'), |c| c == 'd')
-                .as_ref()
-                .map(Rng::text)
+            s.read_char(Character::Char('d')).as_ref().map(Rng::text)
         );
         assert_eq!(
             Ok("f"),
-            s.read_1_with(Token::Char('f'), |c| c == 'f')
-                .as_ref()
-                .map(Rng::text)
+            s.read_char(Character::Char('f')).as_ref().map(Rng::text)
         );
         assert_eq!(
             Err(ScannerError {
                 file: s.source.clone(),
                 pos: 4,
                 msg: None,
-                want: Token::Any,
-                got: Token::EOF
+                want: Character::Any,
+                got: Character::EOF
             }),
-            s.read_1_with(Token::Any, |_| true)
+            s.read_char(Character::Any)
         );
         assert_eq!(Ok(""), s.read_eol().as_ref().map(Rng::text));
     }
@@ -427,17 +402,20 @@ mod test_scanner {
     fn test_read_n() {
         let f = File::mem("asdf");
         let s = Scanner::new(&f);
-        assert_eq!(Ok("as"), s.read_n(2).as_ref().map(Rng::text));
-        assert_eq!(Ok(""), s.read_n(0).as_ref().map(Rng::text));
+        assert_eq!(
+            Ok("as"),
+            s.read_n(2, Character::Any).as_ref().map(Rng::text)
+        );
+        assert_eq!(Ok(""), s.read_n(0, Character::Any).as_ref().map(Rng::text));
         assert_eq!(
             Err(ScannerError {
                 file: s.source.clone(),
                 pos: 4,
                 msg: None,
-                want: Token::Any,
-                got: Token::EOF
+                want: Character::Any,
+                got: Character::EOF
             }),
-            s.read_n(3)
+            s.read_n(3, Character::Any)
         );
         assert_eq!(Ok(""), s.read_eol().as_ref().map(Rng::text));
     }
@@ -451,8 +429,8 @@ mod test_scanner {
                 file: s.source.clone(),
                 pos: 0,
                 msg: None,
-                want: Token::Either(vec![Token::Char('\n'), Token::EOF]),
-                got: Token::Char('a')
+                want: Character::OneOf(vec![Character::Char('\n'), Character::EOF]),
+                got: Character::Char('a')
             }),
             s.read_eol()
         );
@@ -464,26 +442,26 @@ mod test_scanner {
     }
 
     #[test]
-    fn test_read_space1() {
+    fn test_read_space_1() {
         let f = File::mem("  a\t\tb  \nc");
         let s = Scanner::new(&f);
 
-        assert_eq!(Ok("  "), s.read_space1().as_ref().map(Rng::text));
+        assert_eq!(Ok("  "), s.read_space_1().as_ref().map(Rng::text));
         assert_eq!(Ok("a"), s.read_1().as_ref().map(Rng::text));
-        assert_eq!(Ok("\t\t"), s.read_space1().as_ref().map(Rng::text));
+        assert_eq!(Ok("\t\t"), s.read_space_1().as_ref().map(Rng::text));
         assert_eq!(
             Err(ScannerError {
                 file: s.source.clone(),
                 pos: 5,
                 msg: None,
-                want: Token::WhiteSpace,
-                got: Token::Char('b')
+                want: Character::HorizontalSpace,
+                got: Character::Char('b')
             }),
-            s.read_space1()
+            s.read_space_1()
         );
         assert_eq!(Ok("b"), s.read_1().as_ref().map(Rng::text));
-        assert_eq!(Ok("  "), s.read_space1().as_ref().map(Rng::text));
-        assert_eq!(Ok(""), s.read_space1().as_ref().map(Rng::text));
+        assert_eq!(Ok("  "), s.read_space_1().as_ref().map(Rng::text));
+        assert_eq!(Ok(""), s.read_space_1().as_ref().map(Rng::text));
         assert_eq!(Ok("\n"), s.read_eol().as_ref().map(Rng::text));
         assert_eq!(Ok("c"), s.read_1().as_ref().map(Rng::text));
         assert_eq!(Ok(""), s.read_eol().as_ref().map(Rng::text));
